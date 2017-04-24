d0265532d83ca8f7c49c8de0bed0a224d9511875
aweimeow <ccps960718456@gmail.com>
2017-04-24 12:39:58 +0800

    Fix type error, remove 1 unused testcase

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -12,11 +12,10 @@ class TestTriangleCoverageClass:
 
     def test_triangle_c1(self):
         assert triangle(0, 0, 0).type == "NOT TRIANGLE"
-        assert triangle(5, 5, 4).type == "NOT TRIANGLE"
         assert triangle(5, 5, 12).type == "NOT TRIANGLE"
         assert triangle(20, 20, 20).type == "EQUALATERAL"
         assert triangle(12, 15, 9).type == "RIGHT TRIANGLE"
-        assert triangle(100, 50, 100).type == "ISOCELES"
+        assert triangle(100, 50, 100).type == "ISOSCELES"
         assert triangle(100, 102, 101).type == "SCALENE"
 
     def test_triangle_c2(self):
@@ -28,13 +27,13 @@ class TestTriangleCoverageClass:
     def test_triangle_mcdc(self):
         # MCDC: a == b && b == c, FF, FT, TF, TT
         assert triangle(2, 3, 4).type == "SCALENE"
-        assert triangle(4, 5, 5).type == "ISOCELES"
-        assert triangle(5, 5, 4).type == "ISOCELES"
+        assert triangle(4, 5, 5).type == "ISOSCELES"
+        assert triangle(5, 5, 4).type == "ISOSCELES"
         assert triangle(6, 6, 6).type == "EQUALATERAL"
 
         # MCDC: a != b && b == c, FF, FT, TF, TT
         # BTW, in here a, b, c will sorted
-        assert triangle(5, 5, 6).type == "ISOCELES"
+        assert triangle(5, 5, 6).type == "ISOSCELES"
         assert triangle(5, 5, 5).type == "EQUALATERAL"
         assert triangle(3, 5, 6).type == "SCALENE"
-        assert triangle(5, 6, 6).type == "ISOCELES"
+        assert triangle(5, 6, 6).type == "ISOSCELES"
